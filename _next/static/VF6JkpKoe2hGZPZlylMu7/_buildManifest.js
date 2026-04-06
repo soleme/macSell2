a3e133b6c4dfaa6552e9659de9bf7481e85cc51b
soleme <soleme@gmail.com>
2026-04-06 15:52:19 +0900

    Updates

--- _next/static/VF6JkpKoe2hGZPZlylMu7/_buildManifest.js
+++ _next/static/VF6JkpKoe2hGZPZlylMu7/_buildManifest.js
@@ -4,7 +4,7 @@ self.__BUILD_MANIFEST = {
     "beforeFiles": [
       {
         "source": "/macSell2//_next/:path+",
-        "destination": "/_next/:path+"
+        "destination": "/macSell2/_next/:path+"
       }
     ],
     "fallback": []
